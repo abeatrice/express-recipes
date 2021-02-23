0638edcab1c513d65dd01a65b8c75ca1c51bc189
andrew beatrice <abeatrice.mail@gmail.com>
2021-02-23 15:10:05 -0800

    get recipes index for signed in user

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {index, store, show, update, destroy} = require('../controllers/recipesController')
+const {auth} = require('../middleware/auth')
 
-router.get('/', index)
-router.post('/', store)
-router.get('/:id', show)
-router.put('/:id', update)
-router.delete('/:id', destroy)
+router.get('/', auth, index)
+router.post('/', auth, store)
+router.get('/:id', auth, show)
+router.put('/:id', auth, update)
+router.delete('/:id', auth, destroy)
 
 module.exports = router
